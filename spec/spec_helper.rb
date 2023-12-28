9402ecb5d896fbf9a49e15083d95c0d9055caadc
Frank Vielma <frankvielma@gmail.com>
2023-12-27 20:09:02 -0400

    test 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ FactoryBot.reload
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 Capybara.register_driver :non_headless_chrome do |app|
-  args = %w[disable-gpu]
+  args = %w[no-sandbox disable-gpu]
   options = Selenium::WebDriver::Chrome::Options.new(args:)
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
